afacbe75239f7ffffd8b6f6fc859e44beeb921a7
Tiago Peres <psilva.dev@gmail.com>
2021-02-17 20:28:37 -0300

    Adicionado discovery service ao projeto

--- auth-service/src/main/java/com/ecommerce/authservice/AuthServiceApplication.java
+++ auth-service/src/main/java/com/ecommerce/authservice/AuthServiceApplication.java
@@ -2,7 +2,9 @@ package com.ecommerce.authservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class AuthServiceApplication {
     public static void main(String[] args) {
---
+++ discovery-service/src/test/java/com/ecommerce/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.ecommerce.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
