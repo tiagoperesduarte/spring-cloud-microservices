856afdcf41872e511dfd3a76ad21bb8b4f6d128b
Tiago Peres <psilva.dev@gmail.com>
2021-02-17 21:03:59 -0300

    Alteração de nome do microserviço de service discovery

--- eureka-server/src/main/java/com/ecommerce/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/ecommerce/eurekaserver/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package com.ecommerce.registryservice;
+package com.ecommerce.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class RegistryServiceApplication {
+public class EurekaServerApplication {
     public static void main(String[] args) {
-        SpringApplication.run(RegistryServiceApplication.class, args);
+        SpringApplication.run(EurekaServerApplication.class, args);
     }
 }
--- eureka-server/src/test/java/com/ecommerce/eurekaserver/EurekaServerApplicationTests.java
+++ eureka-server/src/test/java/com/ecommerce/eurekaserver/EurekaServerApplicationTests.java
@@ -1,10 +1,10 @@
-package com.ecommerce.registryservice;
+package com.ecommerce.eurekaserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class RegistryServiceApplicationTests {
+class EurekaServerApplicationTests {
     @Test
     void contextLoads() {
     }
